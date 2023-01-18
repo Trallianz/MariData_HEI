f50e85f77a3052e473ad1b28629d4564fd716edc
Jakob <jakob.dumaire@student.uni-luebeck.de>
2023-01-18 16:49:51 +0100

    added icons

--- maridata/src/components/RoutePanel.tsx
+++ maridata/src/components/RoutePanel.tsx
@@ -5,6 +5,16 @@ import {routen_props} from "./../components/Route";
 
 
 const RoutePanel = (props: routen_props) => {
+
+    //split time in hours and minutes
+    const time_array = props.time_driven.split(",")
+    const time_driven_hours = time_array[0];
+    const time_driven_minutes = time_array[1];
+
+    const time_array_anchor = props.time_anchor.split(",")
+    const anchor_hours = time_array_anchor[0];
+    const anchor_minutes = time_array_anchor[1];
+
   return (
     <div className='routen_panel'>
         <div className='routen_panel_div1'>
@@ -36,8 +46,9 @@ const RoutePanel = (props: routen_props) => {
                 <div className='routen_panel_time'>
                     <p>Time:</p>
                     <div className='routen_panel_eco_rating_value'>
-                        <p>{props.time_anchor + props.time_driven}</p>
-                        <p>+38 min</p>
+                        <p>{parseInt(time_driven_hours) + parseInt(anchor_hours)}h </p>
+                        <p>{parseInt(time_driven_minutes) + parseInt(anchor_minutes)}min </p>
+                        <p>+38min</p>
                     </div>
                 </div>
             </div>
--- maridata/src/components/RoutenPanleList.tsx
+++ maridata/src/components/RoutenPanleList.tsx
@@ -7,9 +7,9 @@ import { routen_props } from "./../components/Route";
 const RoutenPanleList = () => {
 
     const route_list = [
-        <RoutenPanel date='1' eco_rating={1} time_driven='1' time_anchor='1' speed={1} ship_type='x' fuel_consumption={1} co2_factor={1} distance={1} cargo_carrying_capacity={1} />,
-        <RoutenPanel date='2' eco_rating={1} time_driven='1' time_anchor='1' speed={1} ship_type='x' fuel_consumption={1} co2_factor={1} distance={1} cargo_carrying_capacity={1} />,
-        <RoutenPanel date='3' eco_rating={1} time_driven='1' time_anchor='1' speed={1} ship_type='x' fuel_consumption={1} co2_factor={1} distance={1} cargo_carrying_capacity={1} />,
+        <RoutenPanel date='1' eco_rating={1} time_driven='11,20' time_anchor='1,10' speed={1} ship_type='x' fuel_consumption={1} co2_factor={1} distance={1} cargo_carrying_capacity={1} />,
+        <RoutenPanel date='2' eco_rating={1} time_driven='12,05' time_anchor='0,35' speed={1} ship_type='x' fuel_consumption={1} co2_factor={1} distance={1} cargo_carrying_capacity={1} />,
+        <RoutenPanel date='3' eco_rating={1} time_driven='11,40' time_anchor='2,05' speed={1} ship_type='x' fuel_consumption={1} co2_factor={1} distance={1} cargo_carrying_capacity={1} />,
     ];
 
     const listItems = route_list.map((route) =>
