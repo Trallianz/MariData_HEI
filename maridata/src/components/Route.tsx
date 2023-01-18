97981c6846cc3c42bad9871360bf231a6837436b
Jakob <jakob.dumaire@student.uni-luebeck.de>
2023-01-18 13:50:21 +0100

    added routen props

--- maridata/src/components/Route.tsx
+++ maridata/src/components/Route.tsx
@@ -1,6 +1,16 @@
 import PropTypes from 'prop-types'
 
-export interface routenProps{
-    time: number;
+export interface routen_props{
+    date: string;
+    eco_rating: number;
+    time_driven: string;
+    time_anchor: string;
+    speed: number;
+    ship_type: string;
+    fuel_consumption: number;
+    co2_factor: number;
+    distance: number;
+    cargo_carrying_capacity: number;
 
 }
+
