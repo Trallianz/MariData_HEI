34736b06a97eafaf281c32332245c1d09d721561
bennet <bennet@bennetbennet.com>
2023-01-20 14:00:35 +0100

    added functionality to buttons in breakout

--- maridata/src/App.tsx
+++ maridata/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className='flex min-h-[1060px] min-w-[1402px] h-screen items-center justify-center'>
       <div className='h-[1040px] w-[1382px] bg-black rounded-lg flex items-center justify-center'>
-        <div className='h-[1024px] w-[1366px]'>
+        <div className='h-[1024px] w-[1366px] bg-gray1'>
           <BrowserRouter>
             <Routes>
               <Route path='/' element={<Map />} />
--- maridata/src/sites/Breakdown.tsx
+++ maridata/src/sites/Breakdown.tsx
@@ -8,7 +8,6 @@ import Leaf from "./../icons/Leaf.svg"
 const Breakdown = () => {
 
   const navigate = useNavigate();
-
   const { state } = useLocation();
   const { props } = state;
 
@@ -18,7 +17,7 @@ const Breakdown = () => {
       <Topbar name={"Breakdown"} />
       <div className='h-[944px] flex justify-between p-8'>
         <div>
-          <button onClick={() => navigate("/comparison")}>
+          <button onClick={() => navigate("/comparison", {state: {props}})}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-[50px]">
               <path fill-rule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clip-rule="evenodd" />
             </svg>
@@ -45,7 +44,9 @@ const Breakdown = () => {
               <EcoRatingTableItem icon={Leaf} attribute="Capacity" cPoints={props.capacity} sPoints={props.capacity} />
             </div>
           </div>
-          <StandardButton label={"How is the Eco-Rating calculated?"} />
+          <div onClick={() => {alert('The Eco-Score is dark magic.')}}>
+            <StandardButton label={"How is the Eco-Rating calculated?"} />
+          </div>
         </div>
       </div>
       <div>
