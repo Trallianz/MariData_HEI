536a9c1b3a1360827c0e67e4813acc9b4725cbc2
bennet <bennet@bennetbennet.com>
2023-01-21 14:42:29 +0100

    Radar can now recieve data

--- maridata/src/components/Radar.tsx
+++ maridata/src/components/Radar.tsx
@@ -7,64 +7,76 @@ Link zum Radar:
 https://www.npmjs.com/package/react-svg-radar-chart
 */
 
-class Radar extends React.Component{ 
- 
-  render() {
+interface tableElements {
+  c_eco: number,
+  c_fuel: number,
+  c_co2: number,
+  c_distance: number,
+  c_capacity: number,
+  s_eco: number,
+  s_fuel: number,
+  s_co2: number,
+  s_distance: number,
+  s_capacity: number,
+}
 
-    const captions = {
-      ecoRating: 'Eco-Rating',
-      fuelConsumption: 'Fuel Consumption',
-      co2Factor: 'CO2 Factor',
-      distance: 'Distance',
-      capacity: 'Capacity'
-    }
+const Radar = ({ c_eco, c_fuel, c_co2, c_distance, c_capacity, s_eco, s_fuel, s_co2, s_distance, s_capacity }: tableElements) => {
 
-    const data = [
-      {
-        data: {
-          ecoRating: 1,
-          fuelConsumption: .8,
-          co2Factor: 0.9,
-          distance: 0.67,
-          capacity: 0.8
-        },
-        meta: { color: '#58FCEC' }
-      },
-      {
-        data: {
-          ecoRating: 0.5,
-          fuelConsumption: .5,
-          co2Factor: 0.9,
-          distance: 0.67,
-          capacity: 0.5
-        },
-        meta: { color: '#0000EC' }
-      },
-    ]
-
-    const options = {
-      scales: 10,
-      scaleProps: () => ({ className: 'fill-gray2 stroke-gray1', fill: 'none' }),
-      captionMargin: 60,
-      captionProps: () => ({
-        className: 'fill-text0',
-        textAnchor: 'middle',
-        fontSize: 20,
-        fontFamily: 'sans-serif'
-      }),
-      wrapCaptionAt: 16,
-    }
-
-
-    return (
-      <RadarChart
-        captions={captions}
-        data={data}
-        size={700}
-        options={options}
-      />
-    );
+  const captions = {
+    ecoRating: 'Eco-Rating',
+    fuelConsumption: 'Fuel Consumption',
+    co2Factor: 'CO2 Factor',
+    distance: 'Distance',
+    capacity: 'Capacity'
   }
+
+  const data = [
+    {
+      //the data of the current route
+      data: {
+        ecoRating: c_eco / 10,
+        fuelConsumption: c_fuel / 10,
+        co2Factor: c_co2 / 10,
+        distance: c_distance / 10,
+        capacity: c_capacity / 10
+      },
+      meta: { color: '#58FCEC' }
+    },
+    {
+      //the data of the suggested routed
+      data: {
+        ecoRating: s_eco / 10,
+        fuelConsumption: s_fuel / 10,
+        co2Factor: s_co2 / 10,
+        distance: s_distance / 10,
+        capacity: s_capacity / 10
+      },
+      meta: { color: '#0000EC' }
+    },
+  ]
+
+  const options = {
+    scales: 10,
+    scaleProps: () => ({ className: 'fill-gray2 stroke-gray1', fill: 'none' }),
+    captionMargin: 60,
+    captionProps: () => ({
+      className: 'fill-text0',
+      textAnchor: 'middle',
+      fontSize: 20,
+      fontFamily: 'sans-serif'
+    }),
+    wrapCaptionAt: 16,
+  }
+
+
+  return (
+    <RadarChart
+      captions={captions}
+      data={data}
+      size={700}
+      options={options}
+    />
+  );
 }
 
 export default Radar;
