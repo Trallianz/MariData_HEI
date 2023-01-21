1e5b2c7c3084885fa971db791ac7c768b297b853
Jakob <jakob.dumaire@student.uni-luebeck.de>
2023-01-21 21:21:43 +0100

    added fixes

--- maridata/src/sites/Map.tsx
+++ maridata/src/sites/Map.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Navigate, useNavigate} from "react-router-dom";
 import "./Map.css";
 import RoutenPanel from "./../components/RoutePanel";
@@ -10,6 +10,7 @@ import line_map_green from "./../icons/line_map_green.svg";
 import line_map_purpel from "./../icons/line_map_purpel.svg";
 import line_map_blue from "./../icons/line_map_blue.svg";
 import location from "./../icons/location.svg";
+import placeholder from "./../icons/location.svg";
 
 interface route_list{
   //TODO find types
@@ -22,23 +23,24 @@ const Map = () => {
 
 const navigate = useNavigate();
 
+//hook to pass the route color down to child components so the according route can be hidden
+const [colorBool, setColorBool] = useState([false, false, false, false]);
+
+
   return (
     <div className='map'>
-      <Link to="/breakdown">breakdown</Link>
-      <Link to="/comparison">comparison</Link>
-      <button onClick={() => navigate("/breakdown")}>breakdown</button>
       <div className='map_map'>
         <img src={land_top} alt="" id='land_top'/>
         <img src={land_botten} alt="" id='land_botton'/>
         <img src={line_map_black} alt="" id='line_map_black'/>
-        <img src="" alt="" id='line_map_green'/>
-        <img src="" alt="" id='line_map_purpel'/>
-        <img src="" alt="" id='line_map_blue'/>
-        <img src="" alt="" id='location_top'/>
-        <img src="" alt="" id='location_botten'/>
+        <img src={line_map_green} alt="" id='line_map_green' style={ {opacity: colorBool[1] ? 0 : 1}}/>
+        <img src={line_map_purpel} alt="" id='line_map_purpel' style={ {opacity: colorBool[3] ? 0 : 1}}/>
+        <img src={line_map_blue} alt="" id='line_map_blue' style={ {opacity: colorBool[2] ? 0 : 1}}/>
+        <img src={location} alt="" id='location_top'/>
+        <img src={location} alt="" id='location_botten'/>
       </div>
       <div className='map_panel'>
-        <RoutenPanelList/>
+        <RoutenPanelList colorBool={colorBool} setColorBool={setColorBool}/>
       </div>
     </div>
   )
