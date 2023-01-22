490920b7afbefa9c0baebf5b3eaf784b873951ac
bennet <bennet@bennetbennet.com>
2023-01-22 15:51:56 +0100

    fixed dropdown for real this time

--- maridata/src/components/Dropdown.tsx
+++ maridata/src/components/Dropdown.tsx
@@ -1,23 +1,57 @@
-import React from 'react'
 import './Dropdown.css'
 
 interface DropdownProps{
-    currentSelect: any;
     setCurrentSelect: any;
+    setRoutes: any;
+    route_list: Array<any>;
+    setIsMenuVisible: any;
 }
 
-const Dropdown = (props: DropdownProps) => {
+const Dropdown = ({setCurrentSelect, setRoutes, route_list, setIsMenuVisible}: DropdownProps) => {
+
+
+    let currentSelect2: string = "eco-score";
 
     const handleClick= () => {
-        props.setCurrentSelect("eco-score");
+        setIsMenuVisible(true);
+        setCurrentSelect("eco-score");
+        currentSelect2 = "eco-score";
+        sort_route_panel_list();
     }
 
     const handleClick2= () => {
-        props.setCurrentSelect("time");
+        setIsMenuVisible(true);
+        setCurrentSelect("time");
+        currentSelect2 = "time";
+        sort_route_panel_list();
     }
 
     const handleClick3= () => {
-        props.setCurrentSelect("date");
+        setIsMenuVisible(true);
+        setCurrentSelect("date");
+        currentSelect2 = "date";
+        sort_route_panel_list();
+    }
+
+
+    //function that sorts the route list after the selected parameter
+    const sort_route_panel_list = () => {
+        if (currentSelect2 === "eco-score") {
+            let route_list_eco = route_list.sort((a, b) => (a.eco_rating > b.eco_rating) ? -1 : 1);
+            setRoutes(route_list_eco);
+
+        } else if (currentSelect2 === "time") {
+            let route_list_time = route_list.sort((a, b) => (
+                (parseInt(a.time_driven.split(",")[0]) * 60 + parseInt(a.time_anchor.split(",")[0]) * 60 + parseInt(a.time_driven.split(",")[1]) + parseInt(a.time_anchor.split(",")[1]))
+                < (parseInt(b.time_driven.split(",")[0]) * 60 + parseInt(b.time_anchor.split(",")[0]) * 60 + parseInt(b.time_driven.split(",")[1]) + parseInt(b.time_anchor.split(",")[1]))) ? -1 : 1);
+            setRoutes(route_list_time);
+            console.log(route_list_time);
+            
+
+        } else if (currentSelect2 === "date") {
+            let route_list_date = route_list.sort((a, b) => (a.eco_rating > b.eco_rating) ? 1 : -1);
+            setRoutes(route_list_date);
+        }
     }
 
     return (
--- maridata/src/components/RoutenPanelList.tsx
+++ maridata/src/components/RoutenPanelList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import RoutenPanel from "./RoutePanel";
 import CurrentRoute from './CurrentRoute';
 import Dropdown from './Dropdown';
@@ -21,38 +21,43 @@ const RoutenPanelList = (props: RoutenPanelList) => {
     ];
 
 
-    //for each route in the route map a div containing the route will be created
-    //"key={index}" bitte nicht löschen, wird gebraucht, sonst gibt react in der Console einen Error
 
 
     //hook that displays the route list when it updates
     const [routes, setRoutes] = useState(route_list);
 
-    //function that sorts the route list after the selected parameter
-    const sort_route_panel_list = () => {
-        if (currentSelect === "eco-score") {
-            let route_list_eco = route_list.sort((a, b) => (a.eco_rating > b.eco_rating) ? -1 : 1);
-            console.log(route_list_eco);
-            setRoutes(route_list_eco);
-
-        } else if (currentSelect === "time") {
-
-            let route_list_time = route_list.sort((a, b) => (
-                (parseInt(a.time_driven.split(",")[0]) * 60 + parseInt(a.time_anchor.split(",")[0]) * 60 + parseInt(a.time_driven.split(",")[1]) + parseInt(a.time_driven.split(",")[1]))
-                > (parseInt(b.time_driven.split(",")[0]) * 60 + parseInt(b.time_anchor.split(",")[0]) * 60 + parseInt(b.time_driven.split(",")[1]) + parseInt(b.time_driven.split(",")[1]))) ? -1 : 1);
-            setRoutes(route_list_time);
-
-        } else if (currentSelect === "date") {
-            let route_list_date = route_list.sort((a, b) => (a.eco_rating > b.eco_rating) ? 1 : -1);
-            setRoutes(route_list_date);
-        }
-    }
 
     //hook that displays the dropdown menu on click
     const [isMenuVisible, setIsMenuVisible] = useState(false);
 
     //hook for the currenctly selected value in the dropdown menu
     const [currentSelect, setCurrentSelect] = useState("eco-score");
+
+
+function renderMap(){
+    return routes.map((route, index) =>
+    <RoutenPanel
+        key={index}
+        date={route.date}
+        eco_rating={route.eco_rating}
+        time_driven={route.time_driven}
+        time_anchor={route.time_anchor}
+        speed={route.speed}
+        ship_type={route.ship_type}
+        fuel_consumption={route.fuel_consumption}
+        co2_factor={route.co2_factor}
+        distance={route.distance}
+        capacity={route.capacity}
+        route_color={route.route_color}
+        colorBool={route.colorBool}
+        setColorBool={route.setColorBool} />
+)
+}
+
+useEffect(() => {
+    renderMap();
+}, [currentSelect]);
+
 
     return (
         <div className='route_panel_list_routes'>
@@ -61,33 +66,21 @@ const RoutenPanelList = (props: RoutenPanelList) => {
                 <div className='route_panel_divider'>
 
                 </div>
-                <div className='route_panel_dropdown' onClick={() => setIsMenuVisible(!isMenuVisible)}>
+                <button className='route_panel_dropdown' onClick={() => setIsMenuVisible(!isMenuVisible)}>
                     <div className='divider2'></div>
-                    <button onClick={() => sort_route_panel_list()}>{currentSelect}</button>
-                    {isMenuVisible && <Dropdown currentSelect={currentSelect} setCurrentSelect={setCurrentSelect} />}
-                </div>
+                    {currentSelect}
+                    {isMenuVisible && <Dropdown setCurrentSelect={setCurrentSelect} setRoutes={setRoutes} route_list={route_list} setIsMenuVisible={setIsMenuVisible}/>}
+                </button>
             </div>
             <div className='current_route_div'>
                 <CurrentRoute date='28.02.2022' eco_rating={6.2} time_driven='11,20' time_anchor='1,10' speed={6} ship_type='x' fuel_consumption={1} co2_factor={1} distance={1} capacity={1} route_color='black' />
             </div>
             <div className='alternative_routes'>
-            { routes.map((route, index) =>
-                <RoutenPanel
-                    key={index}
-                    date={route.date}
-                    eco_rating={route.eco_rating}
-                    time_driven={route.time_driven}
-                    time_anchor={route.time_anchor}
-                    speed={route.speed}
-                    ship_type={route.ship_type}
-                    fuel_consumption={route.fuel_consumption}
-                    co2_factor={route.co2_factor}
-                    distance={route.distance}
-                    capacity={route.capacity}
-                    route_color={route.route_color}
-                    colorBool={route.colorBool}
-                    setColorBool={route.setColorBool} />
-                )}
+
+
+                { //for each route in the route map a div containing the route will be created
+                    //"key={index}" bitte nicht löschen, wird gebraucht, sonst gibt react in der Console einen Error
+                    renderMap()}
             </div>
         </div>
     )
