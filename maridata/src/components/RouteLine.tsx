652dc13b55587f93acdfa7016442a051924b488b
bennet <bennet@bennetbennet.com>
2023-01-28 15:56:44 +0100

    fixed RouteLine

--- maridata/src/components/RouteLine.tsx
+++ maridata/src/components/RouteLine.tsx
@@ -1,15 +1,15 @@
 const RouteLine = (props: { color: string }) => {
     if (props.color == "black") {
-        return <div className='h-[4px] border-dashed border-black border-2'></div>
+        return <div className='h-[4px] border-dotted border-t-4 border-black'></div>
     }
     else if (props.color == "green") {
-        return <div className='h-[4px] border-dashed border-rGreen border-2'></div>
+        return <div className='h-[4px] border-dotted border-t-4 border-rGreen'></div>
     }
     else if (props.color == "blue") {
-        return <div className='h-[4px] border-dashed border-rBlue border-2'></div>
+        return <div className='h-[4px] border-dotted border-t-4 border-rBlue'></div>
     }
     else if (props.color == "purple") {
-        return <div className='h-[4px] border-dashed border-rPurple border-2'></div>
+        return <div className='h-[4px] border-dotted border-t-4 border-rPurple'></div>
     }
     else {
         console.log("Error in RouteLine.tsx");
