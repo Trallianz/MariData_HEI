4b1436e5c2414b16139908bf7d938f39ebcbe589
bennet <bennet@bennetbennet.com>
2023-02-08 12:13:17 +0100

    removed borders for iPad View

--- maridata/src/App.tsx
+++ maridata/src/App.tsx
@@ -7,27 +7,19 @@ import { ShipProvider } from "./ShipContext";
 
 function App() {
 
-
   return (
-    <div className='flex min-h-[1060px] min-w-[1402px] h-screen items-center justify-center'>
-      <div className='h-[1040px] w-[1382px] bg-black rounded-lg flex items-center justify-center'>
-        <div className='h-[1024px] w-[1366px] bg-gray1'>
-          <ShipProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route path='/' element={<Map />} />
-                <Route path='/comparison' element={<Comparison />} />
-                <Route path='/details' element={<Details />} />
-              </Routes>
-            </BrowserRouter>
-          </ShipProvider>
-        </div>
-      </div>
+    <div className='h-[1024px] w-[1366px] bg-gray1'>
+      <ShipProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Map />} />
+            <Route path='/comparison' element={<Comparison />} />
+            <Route path='/details' element={<Details />} />
+          </Routes>
+        </BrowserRouter>
+      </ShipProvider>
     </div>
-
-
   );
 }
-
 
 export default App;
